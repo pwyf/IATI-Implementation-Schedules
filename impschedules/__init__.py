43e166333faf027b8b73c710a5397d3e9d717918
Mark Brough <mark.brough@publishwhatyoufund.org>
2014-04-03 17:08:09 +0100

    Adjust references to user table

--- impschedules/__init__.py
+++ impschedules/__init__.py
@@ -52,7 +52,7 @@ def setup():
     username = "admin"
     password = app.config["ADMIN_PASSWORD"]
     admin = 1
-    u = models.User(username,password,admin)
+    u = models.ImpSchedUser(username,password,admin)
     db.session.add(u)
     db.session.commit()
 
@@ -905,7 +905,7 @@ def login():
     if (request.method=='POST'):
         username = escape(request.form['username'])
         password = escape(request.form['password'])
-        getuser = models.User.query.filter_by(username=username).first()
+        getuser = models.ImpSchedUser.query.filter_by(username=username).first()
         if ((getuser) and (getuser.check_password(password))):
             session['username'] = escape(request.form['username'])
             session['user_id'] = getuser.id
