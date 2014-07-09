4be2acdb45827a3df1263d9b52eb051e464e7975
markbrough <mark.brough@publishwhatyoufund.org>
2014-07-09 20:34:04 +0100

    Remove redundant lines

--- impschedules/api.py
+++ impschedules/api.py
@@ -101,9 +101,7 @@ def merge_dict(d1, d2):
 @app.route("/api/publishers/<publisher_code>/")
 @app.route("/api/organisations/<publisher_code>/")
 @support_jsonp_publishercode
-def publisher_implementation_data(publisher_code):
-    # Small hack for now...
-    
+def publisher_implementation_data(publisher_code):    
     publisher_code = publisher_redirects.correct_publisher(publisher_code)
     publisher = models.Publisher.query.filter_by(publisher_code_actual=publisher_code).first_or_404()
     impschedule = models.ImpSchedule.query.filter_by(publisher_id=publisher.id).first_or_404()
